af2d59a75749db65f68b6b55810657afa578ce5a
tsbhargav@deloitte.com <tsbhargav@deloitte.com>
2022-02-04 02:22:41 +0530

    added functionalities

---
+++ src/app/dashboard/courses.ts
@@ -0,0 +1,11 @@
+export interface courses{
+  num: number;
+    id:String, 
+      courseCreator: String,
+      courseDescription: String,
+      discount: number,
+      discountValidTill: String,
+      price: number,
+      tags: String[],
+      title: String
+}
